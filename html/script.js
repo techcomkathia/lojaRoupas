39da607731f6d60bcd6960ced839d1ab41cd7eeb
unknown <techcomk@gmail.com>
2024-05-22 12:25:25 -0300

    aula01

---
+++ html/script.js
@@ -0,0 +1,147 @@
+let roupasFemininas = [
+    {
+        nome: 'Calça Casual Comfort',
+        imagem: '/03.imagensProdutos/feminino/roupas/f1.png',
+        qtd: 10,
+        descricao: 'Deslize para o conforto definitivo com nossa Calça Casual Comfort. Feita de uma mistura de algodão e elastano, esta calça se adapta perfeitamente ao seu corpo, proporcionando liberdade de movimento sem comprometer o estilo. Vista-se com confiança e elegância!',
+        preco: 49.99
+    },
+    {
+        nome: 'Camisa Essencial Preta',
+        imagem: './03.imagensProdutos/feminino/roupas/f2.png',
+        qtd: 15,
+        descricao: 'Faça uma declaração de moda clássica com nossa Camisa Essencial Preta. Confeccionada com algodão de alta qualidade, esta camisa oferece um ajuste perfeito e uma sensação luxuosa. Desde reuniões de negócios até noites na cidade, esta camisa é uma escolha versátil e sofisticada.',
+        preco: 29.99
+    },
+    {
+        nome: 'Casaco Elegância Atemporal',
+        imagem: './03.imagensProdutos/feminino/roupas/f3.png',
+        qtd: 8,
+        descricao: 'Entre na temporada com estilo e sofisticação usando nosso Casaco Elegância Atemporal. Criado com uma mistura de lã e poliéster, este casaco oferece conforto e calor sem sacrificar o estilo. Deixe sua marca com este clássico atemporal.',
+        preco: 79.99
+    },
+    {
+        nome: 'Short Jeans Vintage',
+        imagem: './03.imagensProdutos/feminino/roupas/f4.png',
+        qtd: 20,
+        descricao: 'Abraçando o espírito do vintage, nosso Short Jeans Vintage é uma adição indispensável ao seu guarda-roupa. Feito de denim durável, este short oferece conforto durante todo o dia, enquanto o design clássico adiciona um toque de estilo atemporal a qualquer look.',
+        preco: 39.99
+    },
+    {
+        nome: 'Camisa Manga Longa Luxo',
+        imagem: './03.imagensProdutos/feminino/roupas/f5.png',
+        qtd: 12,
+        descricao: 'Experimente o luxo da nossa Camisa Manga Longa Luxo. Feita com algodão egípcio de alta qualidade, esta camisa oferece uma sensação suave e sedosa contra a pele. Perfeita para o escritório ou ocasiões especiais, esta camisa é a definição de elegância.',
+        preco: 34.99
+    },
+    {
+        nome: 'Vestido Estampado Glamour',
+        imagem: './03.imagensProdutos/feminino/roupas/f6.png',
+        qtd: 18,
+        descricao: 'Faça uma entrada dramática em nosso Vestido Estampado Glamour. Fabricado em tecido de seda com uma estampa vibrante, este vestido é garantia de chamar a atenção onde quer que vá. Deixe sua personalidade brilhar com este vestido deslumbrante.',
+        preco: 59.99
+    },
+    {
+        nome: 'Conjunto Linho Frescor',
+        imagem: './03.imagensProdutos/feminino/roupas/f7.png',
+        qtd: 6,
+        descricao: 'Mantenha-se fresca e elegante em nosso Conjunto Linho Frescor. Feito de linho leve e respirável, este conjunto é perfeito para os dias ensolarados de verão. A elegância simples do design faz deste conjunto uma escolha perfeita para qualquer ocasião casual.',
+        preco: 69.99
+    },
+    {
+        nome: 'Camisa Malha Preta Essencial',
+        imagem: './03.imagensProdutos/feminino/roupas/f8.png',
+        qtd: 14,
+        descricao: 'Atualize seu guarda-roupa básico com nossa Camisa Malha Preta Essencial. Fabricada em malha de algodão premium, esta camisa oferece conforto inigualável e estilo sem esforço. Um essencial versátil que combina com qualquer estilo.',
+        preco: 27.99
+    },
+    {
+        nome: 'Short Jeans com Cinto Urban',
+        imagem: './03.imagensProdutos/feminino/roupas/f9.png',
+        qtd: 16,
+        descricao: 'Adicione um toque urbano ao seu visual com nosso Short Jeans com Cinto. Feito de denim de alta qualidade, este short oferece estilo e conforto para o dia a dia. O cinto adiciona um toque extra de estilo, garantindo que você se destaque da multidão.',
+        preco: 44.99
+    },
+    {
+        nome: 'Vestido Amarração Charme',
+        imagem: './03.imagensProdutos/feminino/roupas/f10.png',
+        qtd: 10,
+        descricao: 'Exiba seu charme feminino com nosso Vestido Amarração Charme. Feito de tecido leve e fluído, este vestido é perfeito para os dias quentes de verão. A amarração na cintura cria uma silhueta lisonjeira, enquanto o estilo descontraído o torna ideal para qualquer ocasião casual.',
+        preco: 54.99
+    },
+    {
+        nome: 'Saia Couro Elegância',
+        imagem: './03.imagensProdutos/feminino/roupas/f11.png',
+        qtd: 9,
+        descricao: 'Encante com nossa Saia Couro Elegância. Feita de couro macio e luxuoso, esta saia oferece uma mistura perfeita de sofisticação e estilo. Use com uma blusa simples para um look elegante ou com uma camiseta para um visual mais descontraído.',
+        preco: 37.99
+    },
+    {
+        nome: 'Blusa Linho Cropped Delicadeza',
+        imagem: './03.imagensProdutos/feminino/roupas/f12.png',
+        qtd: 11,
+        descricao: 'Explore a delicadeza do linho com nossa Blusa Linho Cropped. Fabricada com linho de alta qualidade, esta blusa oferece frescor e conforto durante todo o dia. O design cropped adiciona um toque moderno, perfeito para combinar com shorts ou saias de cintura alta.',
+        preco: 31.99
+    },
+    {
+        nome: 'Conjunto Pijama Curto Conforto',
+        imagem: './03.imagensProdutos/feminino/roupas/f13.png',
+        qtd: 7,
+        descricao: 'Desfrute de noites tranquilas e confortáveis com nosso Conjunto Pijama Curto Conforto. Feito de algodão macio e leve, este conjunto é perfeito para relaxar em casa ou para uma escapadela de fim de semana. Sinta o conforto e aconchego com este conjunto encantador.',
+        preco: 29.99
+    },
+    {
+        nome: 'Short Couro Marrom Elegância Urbana',
+        imagem: './03.imagensProdutos/feminino/roupas/f14.png',
+        qtd: 13,
+        descricao: 'Eleve seu estilo com nosso Short Couro Marrom Elegância Urbana. Fabricado em couro de alta qualidade, este short combina elegância e ousadia para criar um visual único. Use com uma blusa simples para um look casual ou com uma camisa para um visual mais polido.',
+        preco: 42.99
+    },
+    {
+        nome: 'Camisa Verde Babados Manga Delicadeza',
+        imagem: './03.imagensProdutos/feminino/roupas/f15.png',
+        qtd: 17,
+        descricao: 'Adicione um toque de delicadeza ao seu guarda-roupa com nossa Camisa Verde Babados Manga. Feita de algodão macio e leve, esta camisa oferece conforto durante todo o dia. Os babados nas mangas adicionam um toque feminino, perfeito para qualquer ocasião.',
+        preco: 36.99
+    },
+    {
+        nome: 'Vestido Verão Azul Brisa',
+        imagem: './03.imagensProdutos/feminino/roupas/f16.png',
+        qtd: 10,
+        descricao: 'Sinta a brisa do verão em nosso Vestido Verão Azul Brisa. Feito de tecido leve e fluído, este vestido é ideal para os dias quentes de verão. O azul vibrante e o design fluido criam uma aparência deslumbrante, perfeita para dias ensolarados e noites descontraídas.',
+        preco: 49.99
+    }
+];
+
+{/* <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> */}
+
+let divRoupas = document.getElementById('roupas')
+
+function montarCardProduto(produto){
+    let card = document.createElement('div')
+    card.classList= 'card'
+    card.style = 'width: 18rem;'
+
+    
+    card.innerHTML= `
+        <img src="${produto.imagem}" class="card-img-top" alt="${produto.nome}">
+        <div class="card-body">
+            <h5 class="card-title">${produto.nome}</h5>
+            <!-- <p class="card-text">${produto.descricao}</p> -->
+            <p class="card-text">R$ ${produto.preco}</p>
+            <a href="#" class="btn btn-primary">Adicionar ao Carrinho</a>
+        </div>
+    `
+    divRoupas.appendChild(card)
+}
+
+
+
+roupasFemininas.forEach((elementoArray)=> montarCardProduto(elementoArray))
