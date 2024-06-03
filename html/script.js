47897c82c8c34ded2e3fb4aea097ccc7e4dcfa8c
Tech com Káthia <125410101+techcomkathia@users.noreply.github.com>
2024-06-03 15:41:25 -0300

    criação de função para renderização de produtos da lista

--- html/script.js
+++ html/script.js
@@ -113,35 +113,217 @@ let roupasFemininas = [
     }
 ];
 
-{/* <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div> */}
+let produtosMasculinos = [
+    {
+        nome: 'Jaqueta Couro Preto',
+        imagem: '/03.imagensProdutos/masculino/roupas/m1.png',
+        qtd: 10,
+        descricao: 'Aventure-se com estilo em nossa Jaqueta Couro Preto. Feita de couro genuíno de alta qualidade, esta jaqueta oferece durabilidade e estilo atemporal. Com um design clássico e detalhes cuidadosamente trabalhados, esta jaqueta é perfeita para adicionar um toque de rebeldia a qualquer look.',
+        preco: 129.99
+    },
+    {
+        nome: 'Camisa Polo Cinza Moderna',
+        imagem: '/03.imagensProdutos/masculino/roupas/m2.png',
+        qtd: 15,
+        descricao: 'Elevando o básico a um novo patamar, nossa Camisa Polo Cinza Moderna é uma escolha versátil para qualquer ocasião. Feita de algodão macio e respirável, esta camisa oferece conforto durante todo o dia. O tom de cinza sutil e o design clássico garantem que você esteja sempre na moda.',
+        preco: 34.99
+    },
+    {
+        nome: 'Calça Jeans Reta Conforto',
+        imagem: '/03.imagensProdutos/masculino/roupas/m3.png',
+        qtd: 8,
+        descricao: 'Encare o dia com confiança em nossa Calça Jeans Reta Conforto. Feita de denim durável e elástico, esta calça oferece conforto e estilo sem sacrificar a liberdade de movimento. O corte reto clássico e a lavagem escura tornam esta calça uma escolha versátil para qualquer ocasião.',
+        preco: 49.99
+    },
+    {
+        nome: 'Camisa Básica Verde Elegância',
+        imagem: '/03.imagensProdutos/masculino/roupas/m4.png',
+        qtd: 20,
+        descricao: 'Adicione um toque de cor ao seu guarda-roupa com nossa Camisa Básica Verde Elegância. Feita de algodão macio e respirável, esta camisa oferece conforto durante todo o dia. O verde vibrante adiciona um toque de frescor a qualquer look, tornando esta camisa perfeita para todas as estações.',
+        preco: 24.99
+    },
+    {
+        nome: 'Calça Jogger Bege Urbana',
+        imagem: '/03.imagensProdutos/masculino/roupas/m5.png',
+        qtd: 12,
+        descricao: 'Mantenha-se confortável e estiloso em nossa Calça Jogger Bege Urbana. Feita de algodão macio e elástico, esta calça oferece conforto durante todo o dia. O design moderno e versátil faz desta calça uma escolha perfeita para qualquer look casual.',
+        preco: 39.99
+    },
+    {
+        nome: 'Calça Social Azul Escura Sofisticação',
+        imagem: '/03.imagensProdutos/masculino/roupas/m6.png',
+        qtd: 18,
+        descricao: 'Eleve seu estilo com nossa Calça Social Azul Escura Sofisticação. Feita de tecido de alta qualidade, esta calça oferece conforto e elegância para qualquer ocasião. O tom de azul escuro e o corte clássico garantem que você esteja sempre impecável.',
+        preco: 59.99
+    },
+    {
+        nome: 'Camisa Preta Básica Versatilidade',
+        imagem: '/03.imagensProdutos/masculino/roupas/m7.png',
+        qtd: 6,
+        descricao: 'Simplesmente elegante, nossa Camisa Preta Básica é uma peça essencial em qualquer guarda-roupa masculino. Feita de algodão macio e respirável, esta camisa oferece conforto durante todo o dia. O preto versátil combina facilmente com uma variedade de looks, tornando esta camisa perfeita para todas as ocasiões.',
+        preco: 29.99
+    },
+    {
+        nome: 'Camisa Social Branca Elegância',
+        imagem: '/03.imagensProdutos/masculino/roupas/m8.png',
+        qtd: 14,
+        descricao: 'Exude elegância com nossa Camisa Social Branca. Feita de algodão de alta qualidade, esta camisa oferece conforto e estilo sem igual. Perfeita para o escritório ou eventos formais, esta camisa é uma escolha versátil para o homem moderno.',
+        preco: 34.99
+    },
+    {
+        nome: 'Calça Social Preta Clássica',
+        imagem: '/03.imagensProdutos/masculino/roupas/m9.png',
+        qtd: 16,
+        descricao: 'Faça uma declaração de estilo com nossa Calça Social Preta Clássica. Feita de tecido de alta qualidade, esta calça oferece conforto e elegância para qualquer ocasião. O preto intenso e o corte clássico garantem que você esteja sempre impecável.',
+        preco: 49.99
+    },
+    {
+        nome: 'Calça Preta Jogger Urbana',
+        imagem: '/03.imagensProdutos/masculino/roupas/m10.png',
+        qtd: 10,
+        descricao: 'Mantenha-se confortável e estiloso em nossa Calça Preta Jogger Urbana. Feita de algodão macio e elástico, esta calça oferece conforto durante todo o dia. O design moderno e versátil faz desta calça uma escolha perfeita para qualquer look casual.',
+        preco: 44.99
+    },
+    {
+        nome: 'Camisa Xadrez Bege Casual',
+        imagem: '/03.imagensProdutos/masculino/roupas/m11.png',
+        qtd: 11,
+        descricao: 'Adicione um toque de estilo descontraído ao seu guarda-roupa com nossa Camisa Xadrez Bege Casual. Feita de algodão macio e respirável, esta camisa oferece conforto e estilo para qualquer ocasião. O padrão xadrez clássico adiciona um toque de personalidade a qualquer look.',
+        preco: 39.99
+    },
+    {
+        nome: 'Colete Preto Modernidade',
+        imagem: '/03.imagensProdutos/masculino/roupas/m12.png',
+        qtd: 17,
+        descricao: 'Acentue seu estilo com nosso Colete Preto Modernidade. Feito de tecido de alta qualidade, este colete oferece estilo e versatilidade para qualquer ocasião. Use sobre uma camisa social para um visual elegante ou sobre uma camiseta para um look casual.',
+        preco: 59.99
+    },
+    {
+        nome: 'Calça Jeans Clara Descontração',
+        imagem: '/03.imagensProdutos/masculino/roupas/m13.png',
+        qtd: 9,
+        descricao: 'Aproveite a descontração do fim de semana com nossa Calça Jeans Clara. Feita de denim leve e macio, esta calça oferece conforto e estilo para qualquer aventura. O tom claro adiciona um toque de frescor ao seu look casual.',
+        preco: 42.99
+    },
+    {
+        nome: 'Camisa Polo Azul Clara Frescor',
+        imagem: '/03.imagensProdutos/masculino/roupas/m14.png',
+        qtd: 13,
+        descricao: 'Mantenha-se fresco e estiloso em nossa Camisa Polo Azul Clara. Feita de algodão macio e respirável, esta camisa oferece conforto durante todo o dia. O azul claro suave adiciona um toque de frescor ao seu look casual.',
+        preco: 29.99
+    },
+    {
+        nome: 'Bermuda Jeans Descontração',
+        imagem: '/03.imagensProdutos/masculino/roupas/m15.png',
+        qtd: 15,
+        descricao: 'Curta o verão com nossa Bermuda Jeans. Feita de denim leve e macio, esta bermuda oferece conforto e estilo para os dias quentes. O corte moderno e versátil faz desta bermuda uma escolha perfeita para todas as suas aventuras.',
+        preco: 34.99
+    },
+    {
+        nome: 'Bermuda Jeans Azul Escura Despojada',
+        imagem: '/03.imagensProdutos/masculino/roupas/m16.png',
+        qtd: 20,
+        descricao: 'Aventure-se com estilo em nossa Bermuda Jeans Azul Escura Despojada. Feita de denim durável e elástico, esta bermuda oferece conforto e estilo para todas as ocasiões. O tom de azul escuro e o corte moderno tornam esta bermuda perfeita para qualquer aventura.',
+        preco: 39.99
+    }
+]
 
-let divRoupas = document.getElementById('roupas')
 
-function montarCardProduto(produto){
-    let card = document.createElement('div')
-    card.classList= 'card'
-    card.style = 'width: 18rem;'
+//Trasformamos toda a logica de montagem de produtos em uma função. Para passar o array e a div correta 
+//conseguimos generalizar a mesma função para a página de produtos femininos, masculinos ou infantis. 
 
+function montarProdutos(arrayProdutos,idDiv){
+        
+    arrayProdutos.forEach((produto)=>{
+
+
+        // selecionar o elemento pai
+        let divRoupas = document.getElementById(idDiv) //div pai
     
-    card.innerHTML= `
-        <img src="${produto.imagem}" class="card-img-top" alt="${produto.nome}">
+        let divLinha = document.createElement('div') //div filha
+        //adicionando a primeira classe
+        divLinha.classList.add('container','row')
+        //adicionando a segunda classe
+    
+    
+        //adiciando um filho ao pai
+        divRoupas.appendChild(divLinha)// selecionar o elemento pai
+        
+        //div card produto
+        let cardProduto = document.createElement('div')
+        //adicionando as classes card col m-3
+        cardProduto.classList.add('col-3','card','m-3')
+    
+        cardProduto.innerHTML = `
+        <img src="${produto.imagem}" class="card-img-top my-3" 
+        alt="${produto.nome}">
         <div class="card-body">
             <h5 class="card-title">${produto.nome}</h5>
-            <!-- <p class="card-text">${produto.descricao}</p> -->
-            <p class="card-text">R$ ${produto.preco}</p>
-            <a href="#" class="btn btn-primary">Adicionar ao Carrinho</a>
+            <p class="card-text"> ${produto.preco}</p>
+            <button href="#" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#masculino${idProduto}"> Ver detalhes</button>
+            <a href="#" class="btn btn-primary">Adicionar ao carrinho</a>        
         </div>
-    `
-    divRoupas.appendChild(card)
+        `
+        //div linha sendo o elemento pai de cada card de produto.
+        divLinha.appendChild(cardProduto)
+    
+        //criação do modal
+        let divModal = document.createElement('div')
+        divModal.innerHTML = `
+        <div class="modal fade" id="masculino${idProduto}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5" id="staticBackdropLabel">Detalhes Produto</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <!-- conteúdo no formato de tags -->
+            <!-- nome do produto -h5
+            descrição -p
+            preço- h6
+        -->
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="${produto.imagem}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="${produto.imagem}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="${produto.imagem}" class="d-block w-100" alt="...">
+            </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    
+            <h5>${produto.nome}</h5>
+            <p>${produto.descricao}</p>
+            <h6>R$ ${produto.preco}</h6>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            <button type="button" class="btn btn-primary">Adicionar ao carrinho</button>
+        </div>
+        </div>
+        </div>
+        </div>
+        `
+    
+        divLinha.appendChild(divModal)
+        idProduto+=1
+    
+    })
+    
+
 }
 
-
-
-roupasFemininas.forEach((elementoArray)=> montarCardProduto(elementoArray))
+montarProdutos(produtosMasculinos,'roupasMasculinas')
